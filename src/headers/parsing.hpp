5c05ef3094c526935c5261a28e36b0ec1835e3a6
InvestedBrick <julianbrecht25@gmail.com>
2024-09-01 15:50:29 +0200

    Parsing headers for global var declarations

--- src/headers/parsing.hpp
+++ src/headers/parsing.hpp
@@ -38,7 +38,25 @@ private:
 
     bool is_logical_operator(Token_type type);
     bool is_operator(Token_type type);
-    node::_statement* mk_stmt(std::variant<node::_statement_exit*, node::_statement_var_dec*, node::_statement_var_set*, node::_asm_*, node::_statement_scope*, node::_ctrl_statement*, node::_main_scope*, node::_null_stmt*, node::_statement_output*, node::_statement_input*, node::_statement_function*, node::_statement_ret*, node::_statement_pure_expr*, node::_op_equal*,node::_statement_struct*> var);
+    node::_statement* mk_stmt(std::variant<
+      node::_statement_exit*
+    , node::_statement_var_dec*
+    , node::_statement_var_set*
+    , node::_asm_*
+    , node::_statement_scope*
+    , node::_ctrl_statement*
+    , node::_main_scope*
+    , node::_null_stmt*
+    , node::_statement_output*
+    , node::_statement_input*
+    , node::_statement_function*
+    , node::_statement_ret*
+    , node::_statement_pure_expr*
+    , node::_op_equal*
+    , node::_statement_struct*
+    , node::_statement_globals*
+    > var);
+
     inline node::_null_stmt* mk_null_stmt(std::variant<node::_newline*, node::_newfile*, node::_eof*> var);
     inline void line_err(const std::string& err_msg);
     inline Token try_consume(Token_type type, const std::string& err_msg);
@@ -48,20 +66,23 @@ private:
     inline bool peek_type(Token_type type, size_t offset = 0);
     inline std::optional<node::_argument*> try_parse_argument();
 
-public:
-    inline explicit Parser(std::vector<Token> tokens) : m_tokens(std::move(tokens)),
-        m_Allocator(1024 * 1024 * 2) //2 Megabytes
-    {}
     template<typename Node>
     inline void parse_expr_and_set(Node& node, bool isArray = false);
     inline std::optional<node::_boolean_expr*> parse_boolean_expr();
     inline std::optional<node::_logical_stmt*> parse_logical_stmt();
     inline std::optional<node::_term*> parse_term();
     inline std::optional<node::_expr*> parse_expr(int min_prec = 0);
-    inline std::optional<node::_statement_var_dec*> parse_var_dec();
+    inline node::_statement_var_dec* parse_var_dec();
+    inline node::_statement_var_set* parse_var_set();
     inline std::optional<node::_statement_scope*> parse_scope();
     inline std::optional<node::_double_op*> parse_d_op();
-    inline std::optional<node::_statement*>parse_statement();
+    inline std::optional<node::_statement*> parse_statement();
+
+
+public:
+    inline explicit Parser(std::vector<Token> tokens) : m_tokens(std::move(tokens)),
+        m_Allocator(1024 * 1024 * 2) //2 Megabytes
+    {}
     std::optional<node::_program> parse_program();
 };
 
