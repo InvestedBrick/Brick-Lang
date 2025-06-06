32dcc4a52f12c27af943953788e966067f027a2a
InvestedBrick <julianbrecht25@gmail.com>
2025-06-06 18:55:20 +0200

    renamed pre_process_include and added consume_spaces

--- src/headers/preprocessor.hpp
+++ src/headers/preprocessor.hpp
@@ -12,11 +12,12 @@ private:
 	std::string filename;
 	size_t line_counter = 1;
 	size_t m_idx = 0;
-	inline void pre_process_include();
+	inline void pre_process_directives();
 	void rem_included_main_funcs();
 	inline std::optional<char> peek(int offset = 0) const;
 	inline char consume();
 	void line_err(const std::string& err_msg);
+	int consume_spaces();
 public:
 
 	inline explicit PreProcessor(std::string str, std::string filename) : m_str(std::move(str)) { this->filename = filename; }
